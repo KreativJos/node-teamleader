88209d0769098ed8d0f4b404bfc1a2d06723ec08
Kasper Veenvliet <kasper@kreativsoftware.com>
2016-08-03 21:20:45 +0200

    Added promise. Hide .request Added teamleader api end points as functions

--- examples/example_one.js
+++ examples/example_one.js
@@ -8,41 +8,66 @@ var tl = new TeamLeader({
   group: process.env.API_GROUP,
   api_secret: process.env.API_KEY
 });
+//
+// tl.post('getUsers', function (err, result) {
+//   console.log('c', err, result);
+// });
+//
+// tl.post('getUsers').then(function (result) {
+//   console.log('p', result);
+// });
+//
+// tl.getUsers().then(function (result) {
+//   console.log('getUsers', result);
+// });
 
-tl.post('getUsers', function (err, result) {
-  console.log('c', err, result);
-});
 
-tl.post('getUsers').then(function (result) {
-  console.log('p', result);
-});
+//
+// tl.getTickets({
+//   type: 'waiting_for_client'
+// }).then (function (result) {
+//   console.log('getTickets', result);
+// }).catch (function (err) {
+//   console.log(err);
+// });
+//
+//
+// tl.addTicket({
+//   client_email: 'test+t1@test.nl',
+//   client_name: 'Test T1',
+//   subject: 'Web Question',
+//   send_autoreply: 0
+// }).then(function (result) {
+//   var ticketId = result;
+//
+//   tl.addTicketMessage({
+//     ticket_id: ticketId,
+//     message: 'test msg ' + new Date().getTime(),
+//     send_email: 0
+//   })
+// });
 
-tl.getUsers().then(function (result) {
-  console.log('getUsers', result);
-});
 
-
-
-tl.getTickets({
-  type: 'waiting_for_client'
+//29494
+//
+tl.getCustomFieldInfo({
+  custom_field_id: process.env.KANBAN_CF_ID
 }).then (function (result) {
-  console.log('getTickets', result);
+  console.log('getCustomFieldInfo', result);
 }).catch (function (err) {
   console.log(err);
 });
 
 
-tl.addTicket({
-  client_email: 'test+t1@test.nl',
-  client_name: 'Test T1',
-  subject: 'Web Question',
-  send_autoreply: 0
-}).then(function (result) {
-  var ticketId = result;
+tl.getTasks({
+  amount: 100,
+  pageno: 0,
+  selected_customfields: process.env.KANBAN_CF_ID
+}).then (function (result) {
+  console.log('getTasks', result);
 
-  tl.addTicketMessage({
-    ticket_id: ticketId,
-    message: 'test msg ' + new Date().getTime(),
-    send_email: 0
-  })
+}).catch (function (err) {
+  console.log(err);
 });
+
+
